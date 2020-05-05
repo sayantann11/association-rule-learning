4061cde3abf93a1f6231e3fb890ac08ae7c45eba
sayantann11 <64836894+sayantann11@users.noreply.github.com>
2020-05-05 16:04:41 +0530

    Add files via upload

---
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/Apriori_Python/untitled1.py
@@ -0,0 +1,30 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Market_Basket_Optimisation.csv',header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+
+#training apriori on the data set
+from apyori import apriori
+rules  = apriori(transactions, min_support = 0.003, min_confidence = 0.2,min_lift = 3,min_length = 2)
+
+
+result_detailed = []
+
+for i in range(len(results)):
+
+    result_detailed.append('Rule:\t' + str(results[i][0]) +
+
+                        '\nSupport:\t' + str(results[i][1]) +
+
+                        '\nConfidence:\t' + str(results[i][2][0][2]) +
+
+                        '\nLift:\t' + str(results[i][2][0][3]))
+
---
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/untitled0.py
@@ -0,0 +1,9 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Market_Basket_Optimisation.csv')
